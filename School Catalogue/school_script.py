aac36abf912405325b8c1e29eca5c6c802edd398
Volodymyr <110287885+KopchukVolod@users.noreply.github.com>
2023-04-10 12:53:22 +0300

    Update school_script.py

--- School Catalogue/school_script.py
+++ School Catalogue/school_script.py
@@ -14,16 +14,39 @@ class School:
     return self.numberOfStudents
     
   def set_numberOfStudents(self, newNumberOfStudents):
-    self.__numberOfStudents = newNumberOfStudents
+    self.numberOfStudents = newNumberOfStudents
       
   def __repr__(self):
     return f"A {self.level} school named {self.name} with {self.numberOfStudents} students"
     # return print("A {level} school named {name} with {numberOfStudents} students".format(level=self.level, name=self.name, numberOfStudents=self.numberOfStudents))
+class PrimarySchool(School):
+  def __init__(self, name, numberOfStudents, pickupPolicy):
+    super().__init__(name, "primary", numberOfStudents)
+    self.pickupPolicy = pickupPolicy
+  def get_pickupPolicy(self):
+    return self.pickupPolicy
 
-a1 = School("Kyiv", "middle", 30)
+  def __repr__(self):
+    parentRepr = super().__repr__()
+    return parentRepr + " The pickup policy is {pickupPolicy}".format(pickupPolicy=self.pickupPolicy)
+
+class HighSchool(School):
+  def __init__(self, name, numberOfStudents, sportsTeams):
+    super().__init__(name, "high", numberOfStudents)
+    self.sportsTeams = sportsTeams
+  def get_sportsTeams(self):
+    return self.sportsTeams
+
+  def __repr__(self):
+    parentRepr1 = super().__repr__()
+    return parentRepr1 + " have team with {sportsTeams}".format(sportsTeams=self.sportsTeams[1])
+
+a1 = School("Kyiv", "middle", 158)
 print(a1)
-print(a1.get_name())
-print(a1.get_level())
-a1.set_numberOfStudents(100)
-print(a1.get_numberOfStudents())
-
+a2 = PrimarySchool("MySchool", 159, "free")  
+#print(a2.get_pickupPolicy())
+print(a2)
+a3 = HighSchool("'Best school of Volodymyr Kopchuk'", 100, ["Tennis", "Basketball"])
+print(a3)
+a4 =  HighSchool("Codecademy High", 500, ["Tennis", "Basketball"])
+print(a4)
