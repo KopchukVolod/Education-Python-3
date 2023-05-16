7f44eb73be1490d070a532193ed965b416cb4fc7
Volodymyr <110287885+KopchukVolod@users.noreply.github.com>
2023-05-16 13:37:38 +0300

    Create script.py

---
+++ GENERATORS/Generator Pipelines/script.py
@@ -0,0 +1,12 @@
+def course_generator():
+    yield ("Computer Science", 5)
+    yield ("Art", 10)
+    yield ("Business", 15)
+
+def add_five_students(courses):
+   for course, num_students in courses:
+     yield (course,  num_students + 5)
+
+increased_courses =  add_five_students(course_generator())
+for i in increased_courses:
+  print(i)
